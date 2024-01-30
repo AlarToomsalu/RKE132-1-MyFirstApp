94ed4c0e39935db5736f2e497a46fa1f505365d0
Alar Toomsalu <alar.toomsalu@tktk.ee>
2024-01-30 20:58:53 +0200

    greate new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Rakendus küsib kasutajal sisetada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+//String-Sõne
+string userName = Console.ReadLine(); //Ińput
+
+Console.WriteLine("Hello!"+" " + userName+"!"); //output
+//String interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //output
